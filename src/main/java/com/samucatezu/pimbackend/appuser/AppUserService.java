e4006df98c6f9cd4d9c01d189367f335505121d8
Samuel barbosa almeida <barbosasamuel24@gmail.com>
2022-08-07 14:56:54 -0300

    Registration service implemented

--- src/main/java/com/samucatezu/pimbackend/appuser/AppUserService.java
+++ src/main/java/com/samucatezu/pimbackend/appuser/AppUserService.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -17,6 +18,7 @@ public class AppUserService implements UserDetailsService {
             "user with email %s not found";
 
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
     @Override
@@ -26,6 +28,24 @@ public class AppUserService implements UserDetailsService {
                 .orElseThrow(() ->
                         new UsernameNotFoundException(
                                 String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = appUserRepository
+                .findByEmail(appUser.getEmail())
+                .isPresent();
+        if (userExists) {
+            throw new IllegalStateException("email already taken");
+        }
+        String encodedPassword = bCryptPasswordEncoder
+                .encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        // TODO: Send confirmation token
+        return "works";
     }
 
 
--- src/main/java/com/samucatezu/pimbackend/registration/RegistrationService.java
+++ src/main/java/com/samucatezu/pimbackend/registration/RegistrationService.java
@@ -1,10 +1,30 @@
 package com.samucatezu.pimbackend.registration;
 
+import com.samucatezu.pimbackend.appuser.AppUser;
+import com.samucatezu.pimbackend.appuser.AppUserRole;
+import com.samucatezu.pimbackend.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
